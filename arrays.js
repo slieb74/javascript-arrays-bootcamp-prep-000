6a144b4bd87733b09777da1b43f6e400b8b256d8
Sam <sammyliebman@gmail.com>
2018-04-16 22:31:56 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,7 +8,8 @@ function addElementToBeginningOfArray(array, element){
   return [element, ...array]
 }
 
-function destructivelyAddElementToBeginningOfArray(array, element){
+function destructivelyAddElementToBeginningOfArray(array,element)
+{
   return array.unshift(element)
 }
 
@@ -18,4 +19,8 @@ function addElementToEndOfArray(array, element){
 
 function destructivelyAddElementToEndOfArray(array, element){
   return array.push(element)
+}
+
+function accessElementInArray(array, index){
+  return array[index]
 }
