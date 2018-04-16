fe154bf6e55184734508fdbe1d611f6fce96aac9
Sam <sammyliebman@gmail.com>
2018-04-16 22:33:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -24,3 +24,10 @@ function destructivelyAddElementToEndOfArray(array, element){
 function accessElementInArray(array, index){
   return array[index]
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  return array.shift()
+}
+
+
+
